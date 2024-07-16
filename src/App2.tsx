3dad7671e428a817b4d4f99b04b9518b6a30e509
Guillaume Gomez <guillaume.gomez846@gmail.com>
2024-07-16 21:50:03 +0200

    update the design to test à little bit portal

--- src/App2.tsx
+++ src/App2.tsx
@@ -1,8 +1,17 @@
 import * as THREE from 'three'
 import { Canvas, extend } from '@react-three/fiber'
-import { useGLTF, MeshPortalMaterial, CameraControls, Text, Sky, Gltf } from '@react-three/drei'
+import { 
+  useGLTF,
+  MeshPortalMaterial,
+  CameraControls,
+  Text,
+  Sky,
+  Gltf,
+  Box,
+  RoundedBox } from '@react-three/drei'
 import { easing, geometry } from 'maath';
 import Model from "./Model";
+import DepthBG from "./DepthBG";
 
 extend(geometry);
 const GOLDENRATIO = 1.61803398875;
@@ -22,33 +31,51 @@ function App() {
         <Sky />
         <Gltf src="Donut.glb" position={[0, -0.1, 0]}  scale={0.1} />
       </Frame>
-      <CameraControls makeDefault minAzimuthAngle={-Math.PI / 2.5} maxAzimuthAngle={Math.PI / 2.5} minPolarAngle={0.5} maxPolarAngle={Math.PI / 2} />
+      <CameraControls makeDefault /> {/*minAzimuthAngle={-Math.PI / 2.5} maxAzimuthAngle={Math.PI / 2.5} minPolarAngle={0.5} maxPolarAngle={Math.PI / 2}*/} />
     </Canvas>
   );
 }
 export default App;
 
-function Frame({ id, name, author, bg, width = 1, height = GOLDENRATIO, children, ...props }) {
+function Frame({
+  id,
+  name,
+  author,
+  width = 1,
+  height = GOLDENRATIO,
+  depth = 0.5,
+  children, 
+  ...props 
+}) {
   return (
     <group {...props}>
-      <Text color="black" fontSize={0.25} letterSpacing={-0.025} anchorY="top" anchorX="left" lineHeight={0.8} position={[-0.375, 0.715, 0.01]}>
+      <Text 
+        color="red"
+        fontSize={0.25}
+        letterSpacing={-0.025}
+        anchorY="top"
+        anchorX="left"
+        lineHeight={0.8}
+        position={[-0.375, 0.715, depth/2 + 0.01]}>
         {name}
       </Text>
-      <Text color="black" fontSize={0.1} anchorX="right" position={[0.4, -0.659, 0.01]}>
+      <Text color="red" fontSize={0.1} anchorX="right" position={[0.4, -0.659, depth/2 + 0.01]}>
         /{id}
       </Text>
-      <Text color="black" fontSize={0.04} anchorX="left" position={[0.0, -0.677, 0.01]}>
+      <Text color="red" fontSize={0.04} anchorX="left" position={[0.0, -0.677, depth/2 + 0.01]}>
         {author}
       </Text>
       <mesh name={id}>
-        <roundedPlaneGeometry args={[width, height, 0.1]} />
+        <boxGeometry args={[width, height,depth]} />
         <MeshPortalMaterial side={THREE.DoubleSide}>
-        {children}
+          <ambientLight/>
+          <DepthBG width={width} height={height} depth={depth*3} />
+          {children}
         </MeshPortalMaterial>
       </mesh>
       <mesh name={id} position={[0, 0, -0.001]}>
-        <roundedPlaneGeometry args={[width + 0.05, height + 0.05, 0.12]} />
-        <meshBasicMaterial color="black" />
+        <boxGeometry args={[width + 0.05, height + 0.05, depth]} />
+        <meshBasicMaterial color="red" />
       </mesh>
     </group>
   )
