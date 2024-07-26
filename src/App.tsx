3119073b349b885c3bd9287c547491d2633589df
Guillaume Gomez <guillaume.gomez846@gmail.com>
2024-07-26 13:55:08 +0200

    add daisy-ui

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,13 @@ import ThreeJsRendering from "./Components/ThreeJsRendering";
 
 function App() {
   return (
-    <div>
-      <h1>Title</h1>
-      <ThreeJsRendering />
+    <div className="container m-auto">
+      <div className="flex flex-col items-center gap-3">
+        <h1 className="text-3xl font-bold underline">
+          Portal Experiment
+        </h1>
+        <ThreeJsRendering />
+      </div>
     </div>
   );
 }
