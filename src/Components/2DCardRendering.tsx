9a614ec0b2b5cb82bb1442b026d080f24f6c824c
Guillaume Gomez <guillaume.gomez846@gmail.com>
2024-11-30 18:29:24 +0100

    start to design the card

--- src/Components/2DCardRendering.tsx
+++ src/Components/2DCardRendering.tsx
@@ -1,24 +1,28 @@
 import { useState } from "react";
 
 function CardRendering () {
-    const [backgroundColor, setBackgroundColor] = useState<string>("#FF0D09");
+    const [borderBackground , setBorderBackground] = useState<string>("#FF0D09");
+    const [title , setTitle] = useState<string>("Pomyad");
     return (
         <div className="h-screen flex flex-col gap-4 items-center justify-center bg-success p-10">
-            <div class="card w-96 shadow-xl" style={{background: backgroundColor}}>
-              <figure class="px-5 pt-5">
-                <img
-                  src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
-                  alt="Shoes"
-                  class="rounded-xl" />
-              </figure>
-              <div class="card-body items-center text-center">
-                <h2 class="card-title">Shoes!</h2>
-                <p>If a dog chews shoes whose shoes does he choose?</p>
-                <div class="card-actions">
-                  <button class="btn btn-primary">Buy Now</button>
+            <input type="color" value={borderBackground} onChange={(e) => setBorderBackground(e.target.value)} />
+
+            <div className="card w-96 shadow-xl p-4" style={{background: borderBackground}}>
+                <div className="card-body rounded-xl items-center text-black text-center bg-white p-0">
+                    <figure className="">
+                    <img
+                      src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+                      alt="Shoes"
+                      className="rounded-xl" />
+                    </figure>
+                    <h2 className="card-title text-bold">{title}</h2>
+                    <p>If a dog chews shoes whose shoes does he choose?</p>
+                    <div className="card-actions">
+                      <button className="btn btn-primary">Buy Now</button>
+                    </div>
                 </div>
-              </div>
             </div>
+
         </div>
     )
 }
